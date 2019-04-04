d4817bf77728e58c7c3df4bdc99914a7be9031c1
René Rössler <rene@freshx.de>
2019-04-04 12:04:02 +0200

    use a proper codestyle and modern js features, add yarn.lock

--- index.js
+++ index.js
@@ -1,81 +1,62 @@
-var fs = require('fs');
-var temp = require('temp').track();
-var path = require('path');
-var async = require('async');
-var exec = require('child_process').exec;
+'use strict';
 
+const fs = require('fs');
+const temp = require('temp').track();
+const path = require('path');
+const async = require('async');
+const { exec } = require('child_process');
 
-exports.convert = function(document, format, filter, callback) {
+exports.convert = (document, format, filter, callback) => {
     return async.auto({
-        soffice: function(callback) {
-            function paths() {
-                switch (process.platform) {
-                    case 'darwin': return ['/Applications/LibreOffice.app/Contents/MacOS/soffice'];
-                    case 'linux': return ['/usr/bin/libreoffice', '/usr/bin/soffice'];
-                    case 'win32': return [path.join(process.env['PROGRAMFILES(X86)'], 'LIBREO~1/program/soffice.exe'), 
-                        path.join(process.env['PROGRAMFILES(X86)'], 'LibreOffice/program/soffice.exe'),
-                        path.join(process.env['PROGRAMFILES'], 'LibreOffice/program/soffice.exe')];
-                }
+        soffice: (callback) => {
+            let paths = [];
+            switch (process.platform) {
+                case 'darwin': paths = ['/Applications/LibreOffice.app/Contents/MacOS/soffice'];
+                    break;
+                case 'linux': paths = ['/usr/bin/libreoffice', '/usr/bin/soffice'];
+                    break;
+                case 'win32': paths = [
+                    path.join(process.env['PROGRAMFILES(X86)'], 'LIBREO~1/program/soffice.exe'),
+                    path.join(process.env['PROGRAMFILES(X86)'], 'LibreOffice/program/soffice.exe'),
+                    path.join(process.env.PROGRAMFILES, 'LibreOffice/program/soffice.exe'),
+                ];
+                    break;
+                default:
+                    return callback(new Error(`Operating system not yet supported: ${process.platform}`));
             }
 
-            if (!paths(process.platform)) {
-                return callback('Operating system not yet supported: '+ process.platform);
-            }
-
-            return async.map(paths(process.platform), function(path, callback) {
-                return fs.exists(path, function(exists) {
-                    if (exists === true) {
-                        return callback(null, path);
+            return async.filter(
+                paths,
+                (filePath, callback) => fs.access(filePath, err => callback(null, !err)),
+                (err, res) => {
+                    if (res.length === 0) {
+                        return callback(new Error('Could not find soffice binary'));
                     }
 
-                    return callback();
-                });
-            }, function(err, res) {
-                for (var i in res) {
-                    if (res[i]) {
-                        return callback(null, process.platform === 'win32' ? '"' + res[i] + '"' : res[i]);
-                    }
+                    return callback(null, process.platform === 'win32' ? `"${res[0]}"` : res[0]);
                 }
-                return callback('Could not find soffice binary');
-            });
+            );
         },
-        tempDir: function(callback) {
-            return temp.mkdir('libreofficeConvert', function(err, dir) {
-                if (err) {
-                    return callback(err);
-                }
-
-                return callback(null, dir);
-            });
-        },
-        saveSource: ['tempDir', function(callback, results) {
-            return fs.writeFile(path.join(results.tempDir, 'source'), document, callback);
-        }],
-        convert: ['soffice', 'saveSource', function(callback, results) {
-            var command = results.soffice + ' --headless --convert-to '+format;
+        tempDir: callback => temp.mkdir('libreofficeConvert', callback),
+        saveSource: ['tempDir', (results, callback) => fs.writeFile(path.join(results.tempDir, 'source'), document, callback)],
+        convert: ['soffice', 'saveSource', (results, callback) => {
+            let command = `${results.soffice} --headless --convert-to ${format}`;
             if (filter !== undefined) {
-                command += ':"'+filter+'"';
+                command += `:"${filter}"`;
             }
-            command += ' --outdir '+results.tempDir+' '+path.join(results.tempDir, 'source');
+            command += ` --outdir ${results.tempDir} ${path.join(results.tempDir, 'source')}`;
 
-            return exec(command, function (err, stdout, stderr) {
-                if (err) {
-                    return callback(err);
-                }
-
-                return callback();
-            });
+            return exec(command, callback);
         }],
-        loadDestination: ['convert', function(callback, results) {
-            return fs.readFile(path.join(results.tempDir, 'source.'+format), function(err, destination) {
+        loadDestination: ['convert', (results, callback) =>
+            fs.readFile(path.join(results.tempDir, `source.${format}`), (err, destination) => {
                 if (err) {
                     return callback(err);
                 }
 
                 return callback(null, destination);
-            });
-        }]
-    }, function(err, res) {
+            })],
+    }, (err, res) => {
         temp.cleanup();
 
         if (err) {
