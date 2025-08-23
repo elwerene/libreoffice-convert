4c3808eb50b2f552aa0a37b198db34caf7d86fce
Tobani Esan-George <tobanigeorge@gmail.com>
2025-08-23 01:48:05 +0100

    Test with pptx -> pdf

--- tests/convert.test.js
+++ tests/convert.test.js
@@ -8,23 +8,22 @@ var _jest = require('jest'),
 describe('convert', () => {
 
   it('should convert a word document to text', async () => {
-    const source = _path.join(__dirname, '/resources/hello.docx');
-    const fileName = _path.join(__dirname, '/resources/hello.txt');
-    const res = await convertWithOptions(source, 'txt', undefined, { fileName });
+    const source = _path.join(__dirname, '/resources/doc.pptx');
+    const fileName = _path.join(__dirname, '/resources/doc.pdf');
+    const res = await convertWithOptions(source, 'pdf', undefined, { fileName });
     expect(res).toMatch(fileName);
     expect(_fs.existsSync(res)).toBe(true);
-    expect(_fs.readFileSync(res).toString().trim()).toBe('hello');
   });
 
 
-  // it('if an another instance of soffice exists, should convert a word document to text', async () => {
-  //   exec("soffice  --headless")
-  //   // this command create an instance of soffice. This instance will get a failure "Error: source file could not be loaded"
-  //   // but only after we ask a new convert. So this is enought to reproduce fail when an another instance is open
-  //   await new Promise((resolve) => setTimeout(async () => {
-  //     const docx = _path.join(__dirname, '/resources/hello.docx');
-  //     await convert(docx, 'txt', undefined);
-  //     resolve();
-  //   }, 100));
-  // });
+  it('if an another instance of soffice exists, should convert a word document to text', async () => {
+    exec("soffice  --headless")
+    // this command create an instance of soffice. This instance will get a failure "Error: source file could not be loaded"
+    // but only after we ask a new convert. So this is enought to reproduce fail when an another instance is open
+    await new Promise((resolve) => setTimeout(async () => {
+      const docx = _path.join(__dirname, '/resources/hello.docx');
+      await convert(docx, 'txt', undefined);
+      resolve();
+    }, 100));
+  });
 });
